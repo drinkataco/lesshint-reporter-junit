cee7f4ef727f0c1d992f12dde9f89203a5fc5599
Jóhann Fr. Jóhannsson <7oi@trackwell.com>
2018-08-28 17:53:12 +0000

    Adds writeOut function and restructure
    - writeOut function writes out to file if filePath is given. Otherwise to console
    - comment and restructure module
    - update version

--- index.js
+++ index.js
@@ -1,46 +1,106 @@
-module.exports = {
-  name: 'lesshint-reporter-junit',
-  report: (results) => {
-    const resultsByfiles = {};
-    let output = '';
+/**
+ * Additions for writing files borrowed/inspired by lesshint-lint-xml-reporter
+ * https://github.com/llaumgui/lesshint-lint-xml-reporter
+ * which also borrowed from gulp-jshint-xml-file-reporter
+ * https://github.com/lourenzo/gulp-jshint-xml-file-reporter
+ */
+'use strict';
 
-    // Grab each result and key by file name
-    results.forEach((result) => {
-      if (typeof resultsByfiles[result.fullPath] === 'undefined') {
-        resultsByfiles[result.fullPath] = [];
-      }
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 
-      resultsByfiles[result.fullPath].push(result);
+/**
+ * Creates the output dir
+ * @param {String} filePath
+ * @param cb
+ */
+var createDirectory = (filePath, cb) => {
+    var dirname = path.dirname(filePath);
+
+    mkdirp(dirname, function (err) {
+        if (!err) {
+            cb();
+        } else {
+            console.error('Error creating directory: ', err);
+        }
     });
+};
 
-    // Group testcase by file name
-    Object.keys(resultsByfiles).forEach((file) => {
-      const count = resultsByfiles[file].length;
+var reset = () => {
+    exports.out = [];
+};
+reset();
 
-      output += `<testsuite package="org.lesshint" time="0" tests="${count}" errors="${count}" name="${file}">`;
+/**
+ * Formats result into junit testcase format
+ * @param {Object} result
+ */
+var formatTestcase = (result) => {
+  const message = result.message.replace(/"/g, '&quot;');
+  return `
+    <testcase time="0" name="org.lesshint.${result.linter}">
+      <failure message="${message}">
+        <![CDATA[line ${result.line}, col ${result.column}, ${message} (${result.linter})]]>
+      </failure>
+    </testcase>`;
+};
 
-      resultsByfiles[file].forEach((result) => {
-        const message = result.message.replace(/"/g, '&quot;');
+exports.name = 'lesshint-lint-junit-reporter';
 
-        output += `<testcase time="0" name="org.lesshint.${result.linter}">`;
-        output += `<failure message="${message}">`;
-        output += `<![CDATA[line ${result.line}, col ${result.column}, ${message} (${result.linter})]]>`;
-        output += '</failure>';
-        output += '</testcase>';
-      });
+/**
+ * Processes results into a junit report.
+ * @param {Array} results
+ */
+exports.report = (results) => {
+  const resultsByfiles = {};
+  let output = '';
 
-      output += '</testsuite>';
-    });
-
-    if (output !== '') {
-      let xml = '';
-      xml += '<?xml version="1.0" encoding="utf-8"?>';
-      xml += '<testsuites>';
-      xml += output;
-      xml += '</testsuites>';
-
-      console.log(xml);
-      process.exit(1);
+  // Grab each result and key by file name
+  results.forEach((result) => {
+    if (typeof resultsByfiles[result.fullPath] === 'undefined') {
+      resultsByfiles[result.fullPath] = [];
     }
-  },
+
+    resultsByfiles[result.fullPath].push(result);
+  });
+
+  // Group testcase by file name
+  Object.keys(resultsByfiles).forEach((file) => {
+    const count = resultsByfiles[file].length;
+    let suite = resultsByfiles[file].map(formatTestcase).join('');
+    output += `
+  <testsuite package="org.lesshint" time="0" tests="${count}" errors="${count}" name="${file}">${suite}
+  </testsuite>`;
+  });
+
+  if (output !== '') {
+    let xml = `<?xml version="1.0" encoding="utf-8"?>
+<testsuites>
+  ${output}
+</testsuites>`;
+    exports.out.push(xml);
+  }
+};
+
+/**
+ * Writes output to file if filePath is defined. Otherwise it outputs to console.log
+ * @param {String} filePath
+ */
+exports.writeOut = (filePath) => {
+    if (typeof filePath === 'string') {
+      return () => {
+          createDirectory(filePath, () => {
+              var outStream = fs.createWriteStream(filePath);
+              outStream.write(exports.out[0]);
+              reset();
+          });
+      };
+    }
+    else {
+      return () => {
+        console.log(exports.out[0]);
+        reset();
+      };
+    }
 };
